84d4a3f9a8f6b16b8d7d7cdc252258db96d6fd7e
Yasir Akbar <yasir.akbar86@hotmail.com>
2020-03-31 18:51:49 +0100

    Joi npm

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var app = express();
+var Joi = require('joi');
+
 //var risks = require('./json.js');
 
 app.use(express.json());
@@ -15,14 +17,6 @@ const risks = [
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-router.get('/about', (req,res) => {
-  res.send("About Us")
-})
-
-router.get('/contact', (req,res) => {
-  res.send("Contact Us")
-})
 
 //To get risk api
 router.get('/api/risk', (req,res) => {
@@ -39,20 +33,68 @@ router.get('/api/risk/:id', (req,res) => {
 
 //To create and add new risk
 router.post('/api/risk', (req,res) => {
-  if (!req.body.name && !req.body.type && risks.length < 3){
-    //404 Bad request
-    res.status(400).send("Name is required and should be min of 3 letters!");
+
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  const result = Joi.validate(req.body, schema);
+  if (result.error){
+    res.status(400).send(result.error.details[0].message);
     return;
   }
+
   const risk = {
     id: risks.length + 1,
     name: req.body.name,
     type: req.body.type
   };
+
   risks.push(risk);
   res.send(risks);
-})
 
+});
+
+router.put('/api/risk/:id' , (req,res) => {
+
+  const risk =  risks.find(c => c.id === parseInt(req.params.id));
+  if (!risk) res.status(404).send('The given risk is not found');
+
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  const result = Joi.validate(req.body, schema);
+
+  if (result.error){
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  risk.name = req.body.name;
+  res.send(risks)
+
+});
+
+router.delete('/api/risk/:id' , (req,res) => {
+  const risk =  risks.find(c => c.id === parseInt(req.params.id));
+  if (!risk) res.status(404).send('The given risk is not found');
+
+  const index = risks.indexOf(risk);
+  risks.splice(index, 1);
+
+  res.send(risk);
+  
+
+});
+
+function validateRisk(risk)  {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(risk, schema);
+}
 
 
 module.exports = router;
